c86f1bc3c66a64e025db1feebc925f94e123459f
alexodwright <alexodwright@gmail.com>
2026-03-23 14:26:19 +0000

    Tmux navigator, distant, harpoon, nvim tab settings

--- nvim/.config/nvim/lua/config/opts.lua
+++ nvim/.config/nvim/lua/config/opts.lua
@@ -3,6 +3,11 @@ o.guicursor = ""
 o.number = true
 o.spelllang = "en_gb"
 o.spelloptions:append("camel")
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+
 
 vim.diagnostic.config({
   virtual_text = {
