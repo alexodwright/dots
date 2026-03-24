aae18d05f62d69888deb08997fda628658315984
alexodwright <alexodwright@gmail.com>
2026-03-24 21:09:58 +0000

    Telescope previewer -> bat

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,5 +4,26 @@ return {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    opts = function ()
+        local previewers = require("telescope.previewers")
+
+        local bat_previewer = previewers.new_termopen_previewer({
+            get_command = function(entry)
+                return {
+                    "bat",
+                    "--style=numbers,changes",
+                    "--color=always",
+                    "--pager=never",
+                    entry.path or entry.filename,
+                }
+            end,
+        })
+
+        return {
+            defaults = {
+            file_previewer = bat_previewer
+        }
     }
+    end
 }
