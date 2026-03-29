f2b037e2540d0d873e1f0e13589c32684ab39321
alexodwright <alexodwright@gmail.com>
2026-03-29 16:41:09 +0100

    Add jetbrains install, install appearance, some more fixes

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Set exit on error
+set -e
 
 packages=(
 	nvim
@@ -10,7 +13,7 @@ packages=(
 	)
 
 extras=(
-	ripgrep
+	rg
 	tmux
 	yazi
 	eza
@@ -18,19 +21,16 @@ extras=(
 	)
 
 # Package checks
-if [[ ! -x $(command -v stow) ]]; then
-	echo "Stow must be installed before this script can be run"
-	exit 1
-fi 
-
-if [[ ! -x $(command -v git) ]]; then
-	echo "Git must be installed before this script can be run"
-	exit 1
-fi 
+for dep in stow curl git unzip; do
+    if [[ ! -x  $(command -v "$dep") ]]; then
+        echo "$dep must be installed before this script can be run"
+        exit 1
+    fi
+done
 
 # Install tpm
 if [[ ! -d ~/.config/tmux/plugins/tpm ]]; then
-    git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm"
 fi
 
 # Install nvim
@@ -38,6 +38,7 @@ if [[ ! -x $(command -v nvim) ]]; then
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 	sudo rm -rf /opt/nvim-linux-x86_64
 	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+    rm nvim-linux-x86_64.tar.gz
 fi
 
 # Install oh-my-posh
@@ -53,7 +54,7 @@ fi
 # Install fzf
 if [[ ! -x $(command -v fzf) ]]; then
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install
+	~/.fzf/install --all --no-update-rc
 fi
 
 # Install zed
@@ -61,14 +62,35 @@ if [[ ! -x $(command -v zed) ]]; then
 	curl -f https://zed.dev/install.sh | sh
 fi
 
+# Install JetBrainsMono Nerd Font
+if [[ -z $(fc-list | grep -i "JetBrainsMono") ]]; then
+    echo "JetBrainsMono font not found. Installing..."
+    curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+    mkdir -p "$HOME/.local/share/fonts"
+    unzip JetBrainsMono.zip -d "$HOME/.local/share/fonts/JetBrainsMono"
+    fc-cache -fv
+    rm JetBrainsMono.zip
+fi
+
 # Install configuration files
 for app in "${packages[@]}"; do
-	stow -t ~ "$app"
+	stow -t "${HOME}" "$app"
 done
 
-# Finally check for missing packages
+echo ""  
+echo "=== Checking extras ==="
+missing=()  
 for app in "${extras[@]}"; do
-	if ! command -v ${app}; then
-		echo "Still have to install $app"
-	fi
+    if ! command -v "$app" &>/dev/null; then  
+        missing+=("$app")
+    fi
 done
+
+if [[ ${#missing[@]} -gt 0 ]]; then
+    echo "The following extras still need to be installed:"
+    for app in "${missing[@]}"; do
+        echo "  - $app"
+    done
+else
+    echo "All extras are installed!"
+fi
